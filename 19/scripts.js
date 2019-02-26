780f2ec8526883e491b59794dcd3e909a7471562
Yuan <yuanl@nvidia.com>
2019-02-26 18:18:59 +0800

    add 19

---
+++ 19/scripts.js
@@ -0,0 +1,30 @@
+
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(stream => {
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+function capture() {
+    const [width, height] = [video.videoWidth, video.videoHeight];
+    canvas.width = width;
+    canvas.height = height;
+
+    ctx.drawImage(video, 0, 0, width, height);
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download','handsome');
+    link.textContent = 'Download Image';
+    strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
